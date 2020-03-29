61272b3b7b9df700a8cc244951e46f50a3d94f61
sunadoi <y.sunadoi@gmail.com>
2020-03-29 19:21:55 +0900

    create amplify dynamodb graphql

---
+++ src/graphql/queries.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPost = /* GraphQL */ `
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      id
+      title
+      content
+      price
+      rating
+    }
+  }
+`;
+export const listPosts = /* GraphQL */ `
+  query ListPosts(
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        content
+        price
+        rating
+      }
+      nextToken
+    }
+  }
+`;
