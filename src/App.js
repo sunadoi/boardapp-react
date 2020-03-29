0f15d2b0f126df1ab423190d6b0b2cb1c8e4c09f
sunadoi <y.sunadoi@gmail.com>
2020-03-29 21:56:11 +0900

    update src codes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { API, graphqlOperation } from "aws-amplify";
+import { listPosts } from './graphql/queries';
+import { createPost } from './graphql/mutations';
+import { onCreatePost } from './graphql/subscriptions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    posts: [],
+    title: "",
+    content: ""
+  }
+
+  async componentDidMount() {
+    try {
+      const posts = await API.graphql(graphqlOperation(listPosts))
+      console.log('posts: ', posts)
+      this.setState({ posts: posts.data.listPosts.items })
+    } catch (e) {
+      console.log(e)
+    }
+
+    API.graphql(graphqlOperation(onCreatePost)).subscribe({
+      next: (eventData) => {
+        console.log('eventData: ', eventData)
+        const post = eventData.value.data.onCreatePost
+        const posts = [...this.state.posts.filter(content => {
+          return (content.title !== post.title)
+        }), post]
+        this.setState({ posts })
+      }
+    })
+  }
+
+  createPost = async () => {
+    // バリデーションチェック
+    if (this.state.title === '' || this.state.content === '') return
+
+    // 新規登録 mutation
+    const createPostInput = {
+      title: this.state.title,
+      content: this.state.content
+    }
+
+    // 登録処理
+    try {
+      const posts = [...this.state.posts, createPostInput]
+      this.setState({ posts: posts, title: "", content: "" })
+      await API.graphql(graphqlOperation(createPost, { input: createPostInput }))
+      console.log('createPostInput: ', createPostInput)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          タイトル
+        <input value={this.state.title} name="title" onChange={this.onChange}></input>
+        </div>
+        <div>
+          内容
+        <input value={this.state.content} name="content" onChange={this.onChange}></input>
+        </div>
+        <button onClick={this.createPost}>追加</button>
+        {this.state.posts.map((post,idx) => {return <div key={idx}><div>title: {post.title}</div><div>content: {post.content}</div></div>})}
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from "aws-amplify";                     // <--- 追加
+import config from "./aws-exports";                    // <--- 追加
+Amplify.configure(config)                              // <--- 追加
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
